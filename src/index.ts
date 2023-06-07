72349a1cdc25c532e1ae78ad83b3ff74f586a30d
Bidesh <bideshbanerjee444@gmail.com>
2023-06-07 20:38:00 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 
-import express,{ Express } from "express";
+import express from "express";
 
 const app = express();
 
 
+app.get('/',(req,res) => {
+   res.send('Hello Deploy Succesfull');
+})
 
 
 app.listen(3000,() => {
    console.log('listening at PORT 3000');
 })
+
+export default app;
