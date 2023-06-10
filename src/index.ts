7ac85fb2cc9c313a7400c74443f58c9d6a506b4a
Bidesh <bideshbanerjee444@gmail.com>
2023-06-10 11:28:30 +0530

    tokens based authorization added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,137 @@
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+
 
 
 app.get('/',(req,res) => {
    res.send('Hello Deploy Succesfull');
 })
 
+
+type RequestBody = {
+   fullname:string;
+   password:string;
+   email:string;
+} 
+
+type RequestError<T> = {
+  err:T;
+}
+
+type Tokens = {
+   accessToken:string;
+   refreshToken:string;
+}
+
+
+const VerifyRefreshToken = (req:Request<unknown,unknown,RequestBody>,
+                      res:Response<RequestError<'No Cookie Found' | "missing some payload" | VerifyErrors>>,next:NextFunction) => {
+       
+
+        const cookies = req.cookies;  
+        if(!cookies?.jwt) return res.json({err:'No Cookie Found'});
+        
+       jwt.verify(cookies.jwt as string,process.env.REFRESH_TOKEN_SECRET as string, (err,decode) => {
+             
+             if(err) return res.status(401).json({err});
+             
+             const { fullname, password, email } = decode as RequestBody;
+            
+            if(!fullname || !password || !email) 
+               return res.status(401).json({err:'missing some payload'});
+           
+            req.body = {fullname,password,email};
+       })
+
+       next();
+      
+}
+
+
+app.post('/signup',(req:Request<any,Pick<Tokens,'accessToken'> | RequestError<'missing some payload'>,RequestBody>,res) => {
+
+     
+   const f = req.body;     
+
+  if(!f.fullname || !f.password || !f.email) return res.status(400).json({err:'missing some payload'});
+    
+    
+      
+       const accessToken = jwt.sign(
+       f,
+       process.env.ACCESS_TOKEN_SECRET as string,
+       {expiresIn:'45s'})
+      
+       const refreshToken = jwt.sign(
+       f,
+       process.env.REFRESH_TOKEN_SECRET as string,
+       {expiresIn:'1d'})
+     
+       res.cookie('jwt',refreshToken,{
+         httpOnly:true,
+         
+         maxAge:1000 * 60 * 60 * 24
+      })
+       
+
+       res.json({ accessToken });
+
+})
+
+app.post('/login',(req:Request<any,RequestBody | RequestError<VerifyErrors>,Pick<Tokens,'accessToken'>>,res) => {
+
+     
+     
+      
+       // const accessToken = jwt.ve(
+       // {'name':f?.name},
+       // process.env.ACCESS_TOKEN_SECRET as string,
+       // {expiresIn:'30s'})
+        
+   console.log(req.cookies.jwt);
+       jwt.verify(req.body.accessToken,process.env.ACCESS_TOKEN_SECRET as string,(err,decode) => {
+             
+             if(err) return res.status(401).json({err});
+
+             
+            return res.status(200).json(decode as RequestBody);
+
+       })
+      
+      
+     
+      
+
+      
+
+})
+
+app.post('/token',VerifyRefreshToken,(req:Request<any,Pick<Tokens,'accessToken'>,RequestBody>,res:Response) => {
+
+     
+     
+      
+
+        const accessToken = jwt.sign(
+       req.body,
+       process.env.ACCESS_TOKEN_SECRET as string,
+       {expiresIn:'30s'})
+      
+     res.json({accessToken});
+      
+
+      
+
+})
 
 app.listen(3000,() => {
    console.log('listening at PORT 3000');
