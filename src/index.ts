12c695e59f9f0a80f7ef060bf89766b177017f1d
Bidesh <bideshbanerjee444@gmail.com>
2023-06-10 12:05:39 +0530

    tokens based authorization added

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,23 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use('*',function(req, res, next) { //allow cross origin requests
+
+   
+    
+    res.setHeader('Access-Control-Allow-Origin', `http://localhost:5173/${req.baseUrl}`);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+   // res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 
 
